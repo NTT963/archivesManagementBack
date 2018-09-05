78e61af9a6504e1e7487566aebd3ec5144f2134e
sunwujun <1542470166@qq.com>
2018-09-05 13:29:01 +0800

    图文介绍

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # archivesManagementBack
-档案管理系统
+金陵科技学院软件工程档案管理系统
+![image](https://github.com/sunwujun/archivesManagementBack/blob/master/introduceImg/loginPage.PNG)
+
--- out/artifacts/archivesManagementBack/js/manageArchive.js
+++ out/artifacts/archivesManagementBack/js/manageArchive.js
@@ -60,6 +60,7 @@ var Main = {
                                 },
                                 on: {
                                     click: () => {
+                                    	this
                                         this.remove(params.index)
                                     }
                                 }
@@ -106,6 +107,24 @@ var Main = {
     */
     },
     methods: {
+        Toast(msg,duration){
+            duration=isNaN(duration)?3000:duration;
+            var m = document.createElement('div');
+            m.innerHTML = msg;
+            m.style.cssText="width: auto;padding: 20px 20px;min-width: 200px;opacity: 0.5;height: 70px;color: rgb(255, 255, 255);line-height: 30px;text-align: center;border-radius: 5px;position: fixed;top: 40%;left: 40%;z-index: 999999;background: rgb(0, 0, 0);font-size: 17px;";
+            document.body.appendChild(m);
+            setTimeout(function() {
+                var d = 0.5;
+                m.style.webkitTransition = '-webkit-transform ' + d + 's ease-in, opacity ' + d + 's ease-in';
+                m.style.opacity = '0';
+                setTimeout(function() { document.body.removeChild(m) }, d * 1000);
+            }, duration);
+        },
+    	load() {
+//            this.$Message.success('档案上传成功');
+            this.Toast('档案上传成功,等待管理员审核',1000)
+            this.modal = false
+        },
         fileClick() {
             document.getElementById('file').click()
         },
@@ -116,6 +135,98 @@ var Main = {
             } else {
                 return null;
             }
+        },
+        showPreview(el) {
+            console.log(el)
+            this.fileList(el.target);
+        },
+        fileClick() {
+            document.getElementById('file').click()
+        },
+        fileChange(el) {
+            if (!el.target.files[0].size) return;
+            this.fileList(el.target);
+            el.target.value = ''
+        },
+        fileList(fileList) {
+            let files = fileList.files;
+            for (let i = 0; i < files.length; i++) {
+                //判断是否为文件夹
+                if (files[i].type != '') {
+                    this.fileAdd(files[i]);
+                } else {
+                    //文件夹处理
+                    this.folders(fileList.items[i]);
+                }
+            }
+        },
+        //文件夹处理
+        folders(files) {
+            let _this = this;
+            //判断是否为原生file
+            if (files.kind) {
+                files = files.webkitGetAsEntry();
+            }
+            files.createReader().readEntries(function (file) {
+                for (let i = 0; i < file.length; i++) {
+                    if (file[i].isFile) {
+                        _this.foldersAdd(file[i]);
+                    } else {
+                        _this.folders(file[i]);
+                    }
+                }
+            })
+        },
+        foldersAdd(entry) {
+            let _this = this;
+            entry.file(function (file) {
+                _this.fileAdd(file)
+            })
+        },
+        fileAdd(file) {
+            //总大小
+            this.size = this.size + file.size;
+            //判断是否为图片文件
+            if (file.type.indexOf('image') == -1) {
+                file.src = 'wenjian.png';
+                this.imgList.push({
+                    file
+                });
+            } else {
+                let reader = new FileReader();
+                reader.vue = this;
+                reader.readAsDataURL(file);
+                reader.onload = function () {
+                    file.src = this.result;
+                    this.vue.imgList.push({
+                        file
+                    });
+                }
+            }
+        },
+        fileDel(index) {
+            this.size = this.size - this.imgList[index].file.size;//总大小
+            this.imgList.splice(index, 1);
+        },
+        bytesToSize(bytes) {
+            if (bytes === 0) return '0 B';
+            let k = 1000, // or 1024
+                sizes = ['B', 'KB', 'MB', 'GB', 'TB', 'PB', 'EB', 'ZB', 'YB'],
+                i = Math.floor(Math.log(bytes) / Math.log(k));
+            return (bytes / Math.pow(k, i)).toPrecision(3) + ' ' + sizes[i];
+        },
+        dragenter(el) {
+            el.stopPropagation();
+            el.preventDefault();
+        },
+        dragover(el) {
+            el.stopPropagation();
+            el.preventDefault();
+        },
+        drop(el) {
+            el.stopPropagation();
+            el.preventDefault();
+            this.fileList(el.dataTransfer);
         },
         getKindData() {
 
@@ -153,7 +264,12 @@ var Main = {
             // this.tableTitle = this.tableTitle + data
             data = this.zipContent(data)
             this.currentPage = 1
-            if (data < this.pageSize) {
+            
+            if (data.length == 0) {
+            	this.Toast('该目录下尚无档案',1000)
+			}
+            
+            if (data.length < this.pageSize) {
                 this.nowData = data
             } else {
                 this.nowData = data.slice(0, this.pageSize)
